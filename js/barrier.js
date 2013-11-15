f4d7aee3a91765871eea823776ea152d25507f45
mystatic <923888674@qq.com>
2013-11-15 21:20:18 +0800

    add kind to barrier

--- js/barrier.js
+++ js/barrier.js
@@ -1,16 +1,18 @@
 var barrierArray = 
-[[0,0],[1,4],[1,4],
-[0,0],[1,4],[1,2],
+[[0,0],[0,0],[0,0],
+[0,0],[0,0],[0,0],
+[0,0],[0,0],[0,0],
+[0,0],[1,4],[2,2],
 [0,0],[1,2],[0,0],
 [0,0],[1,2],[0,0],
-[0,0],[1,2],[0,0],
+[0,0],[1,2],[1,2],
 [1,2],[1,2],[1,4],
 [0,0],[0,0],[1,2],
 [0,0],[1,4],[0,0],
 [0,0],[0,0],[1,2],
 [0,0],[1,4],[1,0],
-[1,2],[0,0],[1,2],
-[0,0],[1,2],[0,0],
+[3,2],[0,0],[1,2],
+[1,2],[1,2],[0,0],
 [1,6],[1,6],[0,0],
 [0,0],[1,6],[1,6],
 [1,6],[1,6],[0,0],
@@ -18,14 +20,14 @@ var barrierArray =
 [1,6],[1,6],[0,0],
 [0,0],[1,6],[1,6],
 [1,4],[0,0],[1,4],
-[0,0],[1,2],[1,2],
+[0,0],[1,2],[2,2],
 [1,2],[0,0],[0,0],
 [1,4],[0,0],[0,0],
 [0,0],[1,4],[1,4],
 [1,2],[0,0],[1,4]];
 
-function barrier(x,y,width,height,way,speed,status){
-
+function barrier(kind,x,y,width,height,way,speed,status){
+    this.kind = kind;
     this.x = x;
     this.y = y;
     this.width = width;
@@ -38,15 +40,21 @@ function barrier(x,y,width,height,way,speed,status){
 
 
 function createBarrier(){
-    if(fallingdown.b_count == 24){
+    if(fallingdown.b_count == 26*3){
         fallingdown.b_count = 0;
     }
     for(var i = 0; i<3; i++){
         if(barrierArray[fallingdown.b_count][0] == 0){
             fallingdown.b_count++;
            // continue;
-        }else{
-            fallingdown.barrier.push(new barrier(120*i+25,640,70,70,i,barrierArray[fallingdown.b_count][1],0));
+        }else if(barrierArray[fallingdown.b_count][0] == 1){
+            fallingdown.barrier.push(new barrier(1,120*i+25,640,70,70,i,barrierArray[fallingdown.b_count][1],0));
+            fallingdown.b_count++;
+        }else if(barrierArray[fallingdown.b_count][0] == 2){
+            fallingdown.barrier.push(new barrier(2,120*i+50,410,70,70,i,barrierArray[fallingdown.b_count][1],0));
+            fallingdown.b_count++;
+        }else if(barrierArray[fallingdown.b_count][0] == 3){
+            fallingdown.barrier.push(new barrier(3,120*i,410,70,70,i,barrierArray[fallingdown.b_count][1],0));
             fallingdown.b_count++;
         }
     }
@@ -72,9 +80,18 @@ function draw_barrier () {
         fallingdown.barrier[nn].y,
         fallingdown.barrier[nn].width,
         fallingdown.barrier[nn].height);
+       
        fallingdown.barrier[nn].status=(fallingdown.barrier[nn].status+1)%9;
 
        fallingdown.barrier[nn].y = fallingdown.barrier[nn].y - fallingdown.barrier[nn].speed;
+       if(fallingdown.barrier[nn].kind==2){
+         fallingdown.barrier[nn].x = fallingdown.barrier[nn].x - 2;
+       }
+
+       if(fallingdown.barrier[nn].kind==3){
+         fallingdown.barrier[nn].x = fallingdown.barrier[nn].x + 2;
+       }
+
         if (fallingdown.barrier[nn].y < 0) {
             fallingdown.barrier.splice(nn,1);
         }else{
