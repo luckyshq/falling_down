7b04fa54af17653a755d4fd68ef47ca6f6ad1e32
AppleAndApple <1282774356@qq.com>
2013-11-19 23:59:05 +0800

    感觉这个算法不会错的
    但是还是会从后面撞到，画图的问题可能

--- js/hit.js
+++ js/hit.js
@@ -1,52 +1,61 @@
 //after the role hit a barrier
 var hit_div;
 var sensitivity = 20;
+var r_role = 40;
+var r_barrier = 30;
+
 
 function check_role()
 {
     var hh=fallingdown.barrier.length;
     hit_div = document.getElementById("hit");
-    //man.addChild("div");
-    //man.appendChild(div);
     if (role.harmless == 0)
     {
         for(var nn = 0; nn<hh; nn++)
         {
-            if ((fallingdown.barrier[nn].y <= role.y + role.height - sensitivity && fallingdown.barrier[nn].y+fallingdown.barrier[nn].height>role.y + sensitivity)) 
+            if (getDistance(role.x + 44,role.y + 54,fallingdown.barrier[nn].x+35,fallingdown.barrier[nn].y+35,fallingdown.barrier[nn].type)
+                 < getTwice(r_role + r_barrier))
             {
-                if (fallingdown.barrier[nn].x <= role.x + role.width- sensitivity &&fallingdown.barrier[nn].x+fallingdown.barrier[nn].width>role.x + sensitivity) 
+                if(fallingdown.barrier[nn].kind == 1||fallingdown.barrier[nn].kind == 2||fallingdown.barrier[nn].kind == 3) 
                 {
-                //hit_div.setAttribute("display","block");
-                //hit_div.style.display="inline-block";
-                //document.write("<p>hit</p>");
-                    if(fallingdown.barrier[nn].kind == 1||fallingdown.barrier[nn].kind == 2||fallingdown.barrier[nn].kind == 3) 
-                    {
-                            fallingdown.barrier.splice(nn,1);
-                            hh--;
-                            check_sheld();
-                    }
-                    else if(fallingdown.barrier[nn].kind == 10){
-                        fallingdown.barrier.splice(nn,1);
-                         hh--;
-                         innergame();
-                    }
-                   
-
-                    //if (role.sheld == 0) add_sheld();
-
-
-
+                    fallingdown.barrier.splice(nn,1);
+                    hh--;
+                    check_sheld();
+                }
+                else if(fallingdown.barrier[nn].kind == 10)
+                {
+                    fallingdown.barrier.splice(nn,1);
+                    hh--;
+                    innergame();
+                }
                 //得到障碍物类型，判断是否必死型
                 //是就 check_sheld()
                 //不是就change_life()
-
-                };
         
-            };
+            }
         }
     }
     //else   add_harmless();
+}
 
+function getDistance(x1,y1,x2,y2,type)
+{
+    if(type == 2)
+    {
+
+    }
+    if(type == 3)
+    {
+
+    }
+    var res =  getTwice(x1-x2)+ getTwice(y1-y2);
+    return res;
+
+}
+
+function getTwice(x)
+{
+    return (x*x);
 }
 
 function check_item () 
