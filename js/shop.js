17b6afaee71a1f7518d93163c038467abbb0349a
LUKEIN <364904348@qq.com>
2013-11-29 23:54:20 +0800

    修复了商城的BUG

--- js/shop.js
+++ js/shop.js
@@ -3,6 +3,14 @@ function upGradeSkill (kind) {
 		if (localStorage.money >= 1000 && localStorage.shieldTime<40000) {
 			localStorage.shieldTime = Number(localStorage.shieldTime)+5000;
 			localStorage.money = localStorage.money-1000;
+			role.shieldTime = localStorage.shieldTime;
+			
+			var sx=(role.shieldTime-10000)/5000*60;
+        	ctx_shop[0].fillRect(0,0,sx,20);
+        	ctx_shop[0].lineWidth="3";  
+        	ctx_shop[0].beginPath();  
+        	ctx_shop[0].strokeRect(0,0,360,20);
+			
 			$("#s_info").html("升级成功");
 		}else if(localStorage.shieldTime==40000){
 				$("#s_info").html("你已经满级了哦");
@@ -14,6 +22,13 @@ function upGradeSkill (kind) {
 		if (localStorage.money >= 1000 && localStorage.harmlessTime<17000) {
 			localStorage.harmlessTime = Number(localStorage.harmlessTime)+2000;
 			localStorage.money = localStorage.money-1000;
+			role.harmlessTime = localStorage.harmlessTime;
+			
+			var sx=(role.harmlessTime-5000)/2000*60;
+        	ctx_shop[1].fillRect(0,0,sx,20);
+        	ctx_shop[1].lineWidth="3";  
+        	ctx_shop[1].beginPath();  
+        	ctx_shop[1].strokeRect(0,0,360,20);
 			$("#s_info").html("升级成功");
 		}if(localStorage.harmlessTime==17000){
 				$("#s_info").html("你已经满级了哦");
